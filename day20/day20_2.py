396c1d437b3d0aa3b3bc4ea479cb69c77db724a6
Katerina N. Shananina <e.shananina@rambler-co.ru>
2024-12-20 09:40:32 +0300

    Day 20, part 2

---
+++ day20/day20_2.py
@@ -0,0 +1,44 @@
+import sys
+
+TO_SAVE = 100
+CHEAT_SIZE = 20
+
+start = None
+end = None
+path = dict()
+for i, line in enumerate(sys.stdin):
+    for j, c in enumerate(line):
+        if c == '#':
+            continue
+        if c == 'S':
+            start = (i, j)
+        elif c == 'E':
+            end = (i, j)
+        path[(i, j)] = None
+
+if start is None:
+    exit(1)
+cur = start
+i = 0
+while cur != end:
+    path[cur] = i
+    x, y = cur
+    nex = [(z, w) for z, w in ((x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1))
+           if (z, w) in path and path[(z, w)] is None][0]
+    i += 1
+    cur = nex
+path[end] = i
+
+path = {point: path[point] for point in path if path[point] is not None}
+cheats = 0
+for x, y in path:
+    nbrs = [(x + t, y + q)
+            for t in range(-CHEAT_SIZE, CHEAT_SIZE + 1)
+            for q in range(-CHEAT_SIZE, CHEAT_SIZE + 1)
+            if (abs(t) + abs(q) <= CHEAT_SIZE and
+                (x + t, y + q) in path and
+                path[(x + t, y + q)] - path[(x, y)] - (abs(t) + abs(q)) >= TO_SAVE)]
+    cheats += len(nbrs)
+
+print(cheats)
+    
